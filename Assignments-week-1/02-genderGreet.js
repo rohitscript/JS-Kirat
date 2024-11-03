f38dcbceea400dd8d0e44eab0ee268fdbef6d9bc
rohitverse <rohitkr23072001@gmail.com>
2024-11-03 21:26:43 +0530

    find even numbers in an  array

--- Assignments-week-1/02-genderGreet.js
+++ Assignments-week-1/02-genderGreet.js
@@ -1,2 +1,1 @@
 //2. Write a program that greets a person based on their gender. (If else) 
-
---
+++ Assignments-week-1/04-evenArray.js
@@ -0,0 +1,9 @@
+// Write a program prints all the even numbers in an array 
+
+let arr=[20,30,3,5,10];
+
+for(let i=0;i<arr.length;i++){
+    if(arr[i]%2!==0){
+        console.log(arr[i]);
+    }
+}
